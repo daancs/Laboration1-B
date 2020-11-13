215c1aa4668ba26022fcd152c9820d4f0eeac5f1
daancs <dadi.andrason@gmail.com>
2020-11-13 14:30:09 +0100

    Added a Scania class. Added a platform that can be lowered and raised. Added some tests and documentation to Scania. Overrided the method move in scania to take the angle of the platform into consideration.

--- src/Scania.java
+++ src/Scania.java
@@ -1,2 +1,68 @@
+import java.awt.*;
+
+/**
+ * A class that creates an object Scania, which extends the abstract class car.
+ * @author Hanna Adenholm
+ * @author Omar Sulaiman
+ * @author Dadi Andrason
+ */
+public class Scania extends Car{
+
+    private int platformAngle;
 
 
+
+
+    /**
+     * Constructs a Scania99 with 2 doors, engine power 570 and colour blue
+     */
+    public Scania() {
+        super(2, 570, Color.BLUE, "Scania99", Direction.NORTH);
+        stopEngine();
+    }
+
+    @Override
+    public void move() {
+        if(platformAngle == 0) {
+            super.move();
+        }
+    }
+
+    public double speedFactor() {
+        return 1;
+    }
+
+    /**
+     * Gets the current angle of the platform on the truck.
+     * @return the angle of the truck platform.
+     */
+    public int getPlatformAngle() {
+        return platformAngle;
+    }
+
+    /**
+     * Raises the angle of the platform to the value provided
+     * The method only raises the angle if the angle is smaller than the value provided
+     * and if the truck is stationary
+     * @param angle is the angle that the platform is to be raised to
+     */
+    public void raisePlatform(int angle){
+        if (platformAngle < angle && getCurrentSpeed() < 0.001){
+            platformAngle = Math.min(angle,70);
+        }
+    }
+
+    /**
+     * Lowers the angle of the platform on the truck to the value of the parameter angle.
+     * If the angle is higher than the platform's current angle, nothing happens.
+     * If the angle is lower than 0, the platform's new angle is to be set to 0.
+     * @param angle is the angle that the platform is to be lowered to.
+     */
+    public void lowerPlatform(int angle){
+        if (platformAngle > angle) {
+            platformAngle = Math.max(angle, 0);
+        }
+    }
+
+
+}
